b1880e1eeabd3d883adf66b7a0255fc56ad0d1ed
Uzun Paula <paula.uzun2003.com>
2023-07-14 18:09:01 +0300

    Dezvoltare curs valutar

--- CursValutar/CursValutar/Program.cs
+++ CursValutar/CursValutar/Program.cs
@@ -6,7 +6,23 @@ namespace CursValutar
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double euro = 19.20;
+            double dollar = 18.95;
+
+            Console.WriteLine("Bine ați venit la cursul valutar!");
+            Console.WriteLine("--------------------------------");
+
+            Console.WriteLine("Introduceți suma în lei:");
+            double leiAmount = Convert.ToDouble(Console.ReadLine());
+
+            double euroAmount = leiAmount / euro;
+            double dollarAmount = leiAmount / dollar;
+
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine($"Suma în Euro este: {euroAmount:F2}");
+            Console.WriteLine($"Suma în Dolari este: {dollarAmount:F2}");
+
+            Console.ReadLine();
         }
     }
 }
