bc9218c8f129d43ae1cc7761ed0b5aec58fc8d5e
Uzun Paula <paula.uzun2003.com>
2023-07-17 20:37:54 +0300

    Module OOP

---
+++ Homework_OOP/AppBankAccount/Persoana.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppBankAccount
+{
+    public class Persoana
+    {
+        public TipPersoana tip;
+
+        public string identificator;
+    }
+}
