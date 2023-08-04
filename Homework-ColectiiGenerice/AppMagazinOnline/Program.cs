d42b9feee6c41567c92d51872e8f82b67d41f81c
UzunPaula <paula.uzun2003@gmail.com>
2023-08-04 15:04:23 +0300

    Use of generic collections

---
+++ Homework-ColectiiGenerice/AppMagazinOnline/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace AppListaDeCumparaturi
+{
+    /*	Exercițiu: "Lista de Cumpărături"
+    Cerință: Implementați un program care să permită utilizatorului să adauge și să șteargă elemente
+    dintr-o listă de cumpărături. Utilizatorul ar trebui să poată vizualiza lista completă și să poată
+    șterge articolele după ce le-a cumpărat. */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ShoppingList shoppingList = new ShoppingList();
+
+            int option;
+            do
+            {
+                Console.WriteLine("\nMENU");
+                Console.WriteLine("1: Add product");
+                Console.WriteLine("2: View the products from the shopping list");
+                Console.WriteLine("3: Remove product");
+                Console.WriteLine("0: Exit");
+
+                Console.WriteLine("Choose an option: ");
+
+                while (!int.TryParse(Console.ReadLine(), out option))
+                {
+                    Console.WriteLine("Please enter a valid option !");
+                }
+                
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the product name:");
+                        string productName = Console.ReadLine();
+                        shoppingList.AddProduct(productName);
+                        Console.Clear();
+                        break;
+                    case 2:
+                        shoppingList.ShowProducts();
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter the product name which you want to delete:");
+                        string productNameRemove = Console.ReadLine();
+                        shoppingList.RemoveProduct(productNameRemove);
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+                    case 0:
+                        Console.WriteLine("EXIT");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option ! Please try again.");
+                        break;
+                }
+            } while (option != 0);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework-ColectiiGenerice/AppMagazinOnline/ShoppingList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppListaDeCumparaturi
+{
+    internal class ShoppingList
+    {
+        private List<string> shoppingList;
+
+        public ShoppingList()
+        {
+            shoppingList = new List<string>();
+        }
+        public void AddProduct(string productName)
+        {
+            shoppingList.Add(productName);
+        }
+        public void ShowProducts()
+        {
+            if (shoppingList.Count == 0)
+            {
+                Console.WriteLine("The list is empty !");
+            }
+            else
+            {
+                Console.WriteLine("Shopping list: ");
+                foreach (var product in shoppingList)
+                {
+                    Console.WriteLine(product);
+                }
+            }
+        }
+        public void RemoveProduct(string productName)
+        {
+            if (shoppingList.Contains(productName))
+            {
+                shoppingList.Remove(productName);
+                Console.WriteLine($"The product {productName} has been sucessfully removed from the shopping list.");
+            }
+            else
+            {
+                Console.WriteLine($"Error. The product {productName} does not exist in the shopping list !");
+            }
+        }
+    }
+}
